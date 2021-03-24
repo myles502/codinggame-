e2cfad6def8490d703d2c92e859110134acc4be5
myles502 <80232038+myles502@users.noreply.github.com>
2021-03-23 22:19:47 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # codinggame-
+
+This is my coding game, as it states in the directions you click start to begin questions. Your clicks will return a message based on the response and you will get a flashing points message plus addtional time on start button timer if you are correct. 
--- script.js
+++ script.js
@@ -11,29 +11,6 @@ var ansThree = document.querySelector ("#question-three")
 
 var points = [];
 
-var todos = [];
-
-// TODO: What is the purpose of this function? //display todo list just writing this twice
-function renderPoints() {
-  // TODO: Describe the functionality of the following two lines of code. make to do list appear 
-  todoList.innerHTML = ""; //intitalizing todo re setting 
-  todoCountSpan.textContent = todos.length; //display number of todo or the counter updates 
-  
-  // TODO: Describe the functionality of the following `for` loop.
-  for (var i = 0; i < points.length; i++) {
-    var points = points[i]; //for every iteration of for loop adds item by one 
-
-    var li = document.createElement("li");
-    li.textContent = points;
-    li.setAttribute("data-index", i);
-
-    var button = document.createElement("button");
-    button.textContent = "Complete ✔️";
-
-    li.appendChild(button);
-    todoList.appendChild(li);
-  }
-}
 
 
 
@@ -53,7 +30,34 @@ function timer() {
 
 
 ansOne.addEventListener("click", function(event) { 
-  var element = event.target; window.alert ("points!!!!")
+  timerId = setInterval (timer,1000);
+  function timer() {
+    secondsLeft++;
+    if (secondsLeft >= 0 )
+timeEl.textContent = secondsLeft + " points!!!"
+else if (secondsLeft <= 0)
+timeEl.textContent = " YOU LOSE!!!!!!!"
+ } } ) ;
+  
+ansTwo.addEventListener("click", function(event) { 
+  timerId = setInterval (timer,1000);
+  function timer() {
+    secondsLeft--;
+    if (secondsLeft >= 0 )
+timeEl.textContent = secondsLeft + " wrong!!!"
+else if (secondsLeft <= 0)
+timeEl.textContent = " YOU LOSE!!!!!!!"
+ } } ) ;
+  
+ ansThree.addEventListener("click", function(event) { 
+  timerId = setInterval (timer,1000);
+  function timer() {
+    secondsLeft--;
+    if (secondsLeft >= 0 )
+timeEl.textContent = secondsLeft + " very wrong!!!"
+else if (secondsLeft <= 0)
+timeEl.textContent = " YOU LOSE!!!!!!!"
+ } } ) ;
   
 
 
@@ -64,9 +68,6 @@ ansOne.addEventListener("click", function(event) {
 
 
 
-
-
-} ) ;
   
   
 
